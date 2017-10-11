90210cb47f80c0f36e5c7449098c77aed89a5753
Sinan <sinanrad@outlook.com>
2017-10-11 09:51:26 +0200

    andra

---
+++ Project upggift 1/Project upggift 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_upggift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a menu and make 
+            //calls to ShopHandler for search and sort.    
+        }
+    }
+
+
+    /*  
+     Enumerations are special sets of named values 
+     which all maps to a set of numbers, usually integers. 
+     They come in handy when you wish to be able to choose 
+     between a set of constant values. 
+     Category is here used to define category for Items.
+    */
+    public enum Category
+    {
+        Food,
+        Drinks,
+        Bread,
+        Books,
+        Sport
+    }
+}
+
